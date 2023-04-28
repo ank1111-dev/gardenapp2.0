f0517c4c4fb78ca9f5f94f000de54845934fcd03
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-28 16:13:01 +1000

    Added styling by creating a separate css file, added image, search icon, arrow icons

--- src/components/LocationSelection/index.js
+++ src/components/LocationSelection/index.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button, TextField } from '@mui/material';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TextField, Typography, IconButton,  } from "@mui/material";
+import {  ArrowBack as ArrowBackIcon,  ArrowForward as ArrowForwardIcon, Clear as ClearIcon} from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import './index.css'
 
 const Location = () => {
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState("");
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showRetryButton, setShowRetryButton] = useState(false);
   const navigate = useNavigate();
 
-  const handleLocationSubmit = async (e) => {
+  const handleLocationSubmit = (e) => {
     e.preventDefault();
-    if (location.trim() === '') {
+    if (location.trim() === "") {
       setError(true);
+      setErrorMessage("* Location is required");
+      setShowRetryButton(false);
     } else {
       navigate(`/weather-details/${location}`);
     }
@@ -19,28 +26,83 @@ const Location = () => {
   const handleLocationChange = (e) => {
     setLocation(e.target.value);
     setError(false);
+    setErrorMessage("");
+    setShowRetryButton(false);
+
+  };
+
+  const handleRetryButtonClick = () => {
+    setLocation("");
+    setError(false);
+    setErrorMessage("");
+    setShowRetryButton(false);
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-      <form onSubmit={handleLocationSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '70px', backgroundColor: '#f8f8f8', borderRadius: '10px' }}>
-        <h1 style={{ marginBottom: '30px', color: '#b6986d' }}>Enter your location</h1>
-        <TextField
-          label="Location"
-          value={location}
-          onChange={handleLocationChange}
-          variant="outlined"
-          error={error}
-          helperText={error ? '*Location is required' : ''}
-          style={{ marginBottom: '20px', borderColor: error ? '#eb3911' : 'grey', color: '#b6986d' }}
+    <>
+      <div className="header">
+        <IconButton
+          onClick={() => navigate(-1)}
+          className="icon-button icon-button-arrow-left"
+        >
+          <ArrowBackIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => navigate("/gardening-wizard")}
+          className="icon-button icon-button-arrow-right"
+        >
+          <ArrowForwardIcon />
+        </IconButton>
+      </div>
+      <div className="container">
+        <Typography variant="h4" className="subtitle" color="white">
+          Looking for gardening plants suggestions?
+        </Typography>
+        <img
+          src="../image/garden-ideas.jpg"
+          alt="Gardening Suggestions"
+          className="img"
         />
-        <Button variant="contained" type="submit" style={{ backgroundColor: '#4CAF50', color: '#fff' }}>
-          Submit
-        </Button>
-      </form>
-    </div>
+        <Typography variant="h6" className="subtitle">
+          Enter your location and get plant suggestions based on the weather
+          in your area.
+        </Typography>
+        <form onSubmit={handleLocationSubmit}>
+          <div className="form">
+            <TextField
+              label="Enter Location"
+              value={location}
+              onChange={handleLocationChange}
+              variant="outlined"
+              error={error}
+              helperText={error ? errorMessage : ""}
+              className="text-field"
+            />
+            <IconButton
+              type="submit"
+              aria-label="search"
+              className="icon-button"
+            >
+              <SearchIcon />
+            </IconButton>
+            <IconButton
+              onClick={handleRetryButtonClick}
+              aria-label="clear"
+              className={`icon-button retry-button ${
+                showRetryButton ? "show" : ""
+              }`}
+            >
+              <ClearIcon />
+            </IconButton>
+          </div>
+        </form>
+        <Typography variant="body2" className="subtitle1">
+          *Accepts location name or postcode
+        </Typography>
+      </div>
+    </>
   );
-};
+}
 
 export default Location;
-
+    
